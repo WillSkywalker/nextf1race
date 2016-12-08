9a359b83a8f65c3c1a1ac063f4f960c3f3b5cd1f
Will Skywalker <cxbats@gmail.com>
2016-12-08 15:40:21 +0800

    basic concept

--- nextf1race/nextf1race.py
+++ nextf1race/nextf1race.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -_- coding: utf-8 -_-
 
+import sys
 import json
 import datetime
+import argparse
 
-NOW = datetime.datetime.now()
-UTCNOW = datetime.datetime.utcnow()
+NOW = datetime.datetime.utcnow()
+# UTCNOW = datetime.datetime.utcnow()
 
 
 
@@ -27,26 +29,65 @@ class NextF1Race(object):
     def next_race(self):
         NOW = datetime.datetime.utcnow()
         for race in self.calendar:
-            race_date = datetime.datetime(NOW.year, race['date'][0], 
-                                          race['date'][1])
+            circuit = self.circuits[race['circuit']]
+            race_time = list(map(int, circuit['time']['Race'].split(':')))
+            race_date = datetime.datetime(NOW.year, race['date'][0], race['date'][1], 
+                                          race_time[0], race_time[1])
             if race_date > NOW:
-                next_race = race
-                next_race['gap'] = race_date - NOW
+                next = race
+                next['gap'] = race_date - NOW
                 break
         else:
             next = self.calendar[-1]
         next['gap'] = datetime.datetime(NOW.year+1, 
             race['date'][0], race['date'][1]) - NOW
+        next['circuit'] = self.circuits[next['circuit']]
         return next
 
 
+def format_timedelta(timedelta):
+    hours, seconds = divmod(timedelta.seconds, 3600)
+    minutes, seconds = divmod(seconds, 60)
+    timestr = 'In '
+
+    def add_part(name, num):
+        s = ''
+        if num:
+            if num == 1:
+                s += '1 %s ' % name
+            else:
+                s += '%d %ss ' % (num, name)
+        return s
+
+    timestr += add_part('day', timedelta.days)
+    timestr += add_part('hour', hours)
+    timestr += add_part('minute', minutes)
+    timestr += add_part('second', seconds)
+
+    return timestr.strip()
 
 
 def main():
+    parser = argparse.ArgumentParser(description='What\'s the next Formula 1 race?')
+    parser.add_argument('-d', '--detailed', action='store_true')
+    parser.add_argument('-s', '--simplified', action='store_true')
+    args = vars(parser.parse_args())
+
     n = NextF1Race()
     next_race = n.next_race()
+    timestr = format_timedelta(next_race['gap'])
+
 
     print('Next race: %s Grand Prix' % next_race['country'])
+    print(timestr)
+
+    if not args['simplified']:
+        print(next_race['circuit']['time'])
+
+
+    if args['detailed']:
+        print('stay tuned...')
+
 
 
 if __name__ == '__main__':
